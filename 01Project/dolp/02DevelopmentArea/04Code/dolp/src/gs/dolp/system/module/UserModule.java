229bcdcac7face279ff03fea60c9789a8a32f77f
conanca <imconan@gmail.com>
2010-07-01 08:12:13 +0000

    将业务逻辑放到service类中实现

--- 01Project/dolp/02DevelopmentArea/04Code/dolp/src/gs/dolp/system/module/UserModule.java
+++ 01Project/dolp/02DevelopmentArea/04Code/dolp/src/gs/dolp/system/module/UserModule.java
@@ -4,18 +4,9 @@ import gs.dolp.jqgrid.JqgridData;
 import gs.dolp.system.domain.User;
 import gs.dolp.system.service.UserService;
 
-import java.util.List;
-
 import javax.servlet.http.HttpSession;
 
-import org.nutz.dao.Cnd;
-import org.nutz.dao.Condition;
-import org.nutz.dao.pager.Pager;
 import org.nutz.ioc.annotation.InjectName;
-import org.nutz.json.Json;
-import org.nutz.lang.Strings;
-import org.nutz.log.Log;
-import org.nutz.log.Logs;
 import org.nutz.mvc.annotation.At;
 import org.nutz.mvc.annotation.Fail;
 import org.nutz.mvc.annotation.Filters;
@@ -29,42 +20,12 @@ import org.nutz.mvc.annotation.Param;
 public class UserModule {
 
 	private UserService userService;
-	private static final Log log = Logs.getLog(UserModule.class);
 
-	// TODO
-	// 将该方法的内容放到service类中实现
 	@At
 	@Ok("json")
 	public JqgridData<User> getGridData(@Param("page") String page, @Param("rows") String rows,
 			@Param("sidx") String sidx, @Param("sord") String sord) {
-
-		int pageNumber = 1;
-		int pageSize = 10;
-		String sortColumn = "ID";
-		String sortOrder = "ASC";
-		if (!Strings.isEmpty(page)) {
-			pageNumber = Integer.parseInt(page);
-		}
-		if (!Strings.isEmpty(rows)) {
-			pageSize = Integer.parseInt(rows);
-		}
-		if (!Strings.isEmpty(sidx)) {
-			sortColumn = sidx;
-		}
-		if (!Strings.isEmpty(sord)) {
-			sortOrder = sord;
-		}
-		Pager pager = userService.dao().createPager(pageNumber, pageSize);
-		Condition cnd = Cnd.wrap("1=1 ORDER BY " + sortColumn + " " + sortOrder);
-		List<User> list = userService.query(cnd, pager);
-		log.debug(Json.toJson(list));
-		int count = userService.count();
-		int totalPage = count / pageSize + (count % pageSize == 0 ? 0 : 1);
-		JqgridData<User> jq = new JqgridData<User>();
-		jq.setPage(pageNumber);
-		jq.setTotal(totalPage);
-		jq.setRows(list);
-		return jq;
+		return userService.getGridData(page, rows, sidx, sord);
 	}
 
 	@At
@@ -72,12 +33,7 @@ public class UserModule {
 	@Fail("redirect:/1.jsp")
 	@Filters
 	public void login(@Param("num") String number, @Param("pwd") String password, HttpSession session) {
-		Condition cnd = Cnd.where("NUMBER", "=", number).and("PASSWORD", "=", password);
-		User user = userService.fetch(cnd);
-		if (null == user) {
-			throw new RuntimeException("Error username or password");
-		}
-		session.setAttribute("logonUser", user);
+		session.setAttribute("logonUser", userService.userAuthenticate(number, password));
 	}
 
 	@At
@@ -102,9 +58,6 @@ public class UserModule {
 	@At
 	@Fail("json")
 	public void deleteRow(@Param("id") String ids) {
-		if (!Strings.isEmpty(ids)) {
-			Condition cnd = Cnd.wrap("ID IN (" + ids + ")");
-			userService.clear(cnd);
-		}
+		userService.deleteUsers(ids);
 	}
 }
--- 01Project/dolp/02DevelopmentArea/04Code/dolp/src/gs/dolp/system/service/UserService.java
+++ 01Project/dolp/02DevelopmentArea/04Code/dolp/src/gs/dolp/system/service/UserService.java
@@ -1,13 +1,72 @@
 package gs.dolp.system.service;
 
+import gs.dolp.jqgrid.JqgridData;
 import gs.dolp.system.domain.User;
+import gs.dolp.system.module.UserModule;
 
+import java.util.List;
+
+import org.nutz.dao.Cnd;
+import org.nutz.dao.Condition;
 import org.nutz.dao.Dao;
+import org.nutz.dao.pager.Pager;
+import org.nutz.json.Json;
+import org.nutz.lang.Strings;
+import org.nutz.log.Log;
+import org.nutz.log.Logs;
 import org.nutz.service.IdEntityService;
 
 public class UserService extends IdEntityService<User> {
 
+	private static final Log log = Logs.getLog(UserModule.class);
+
 	public UserService(Dao dao) {
 		super(dao);
+	}
+
+	public JqgridData<User> getGridData(String page, String rows, String sidx, String sord) {
+		int pageNumber = 1;
+		int pageSize = 10;
+		String sortColumn = "ID";
+		String sortOrder = "ASC";
+		if (!Strings.isEmpty(page)) {
+			pageNumber = Integer.parseInt(page);
+		}
+		if (!Strings.isEmpty(rows)) {
+			pageSize = Integer.parseInt(rows);
+		}
+		if (!Strings.isEmpty(sidx)) {
+			sortColumn = sidx;
+		}
+		if (!Strings.isEmpty(sord)) {
+			sortOrder = sord;
+		}
+		Pager pager = dao().createPager(pageNumber, pageSize);
+		Condition cnd = Cnd.wrap("1=1 ORDER BY " + sortColumn + " " + sortOrder);
+		List<User> list = query(cnd, pager);
+		log.debug(Json.toJson(list));
+		int count = count();
+		int totalPage = count / pageSize + (count % pageSize == 0 ? 0 : 1);
+		JqgridData<User> jq = new JqgridData<User>();
+		jq.setPage(pageNumber);
+		jq.setTotal(totalPage);
+		jq.setRows(list);
+		return jq;
+	}
+
+	public User userAuthenticate(String number, String password) {
+		Condition cnd = Cnd.where("NUMBER", "=", number).and("PASSWORD", "=", password);
+		User user = fetch(cnd);
+		if (null == user) {
+			throw new RuntimeException("Error username or password");
+		}
+		return user;
+	}
+
+	public void deleteUsers(String ids) {
+		if (!Strings.isEmpty(ids)) {
+			Condition cnd = Cnd.wrap("ID IN (" + ids + ")");
+			clear(cnd);
+		}
 	}
 }
