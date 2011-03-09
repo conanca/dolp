ac90bc80afc4a46846ededaeaa6a8d662a276f87
conanca <imconan@gmail.com>
2011-03-09 11:13:20 +0000

    修正cause为null时的bug

--- 01Project/hotel/02DevelopmentArea/04Code/dolpinhotel/src/gs/dolp/common/util/ExceptionHandler.java
+++ 01Project/hotel/02DevelopmentArea/04Code/dolpinhotel/src/gs/dolp/common/util/ExceptionHandler.java
@@ -10,13 +10,15 @@ public class ExceptionHandler {
 			sb.append("\n");
 		}
 		Throwable cause = exception.getCause();
-		sb.append("Caused by: ");
-		sb.append(cause.toString());
-		sb.append("\n");
-		for (StackTraceElement ele : cause.getStackTrace()) {
-			sb.append("\t");
-			sb.append(ele.toString());
+		if (cause != null) {
+			sb.append("Caused by: ");
+			sb.append(cause.toString());
 			sb.append("\n");
+			for (StackTraceElement ele : cause.getStackTrace()) {
+				sb.append("\t");
+				sb.append(ele.toString());
+				sb.append("\n");
+			}
 		}
 		return sb.toString();
 	}
