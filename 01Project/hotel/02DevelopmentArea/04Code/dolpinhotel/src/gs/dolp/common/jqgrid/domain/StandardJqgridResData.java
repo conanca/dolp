c4b506c1d970790da21377f8d83a91be797de86a
conanca <imconan@gmail.com>
2011-05-27 18:47:10 +0800

    增加方法：用于获取指定字段的值的数组

--- 01Project/hotel/02DevelopmentArea/04Code/dolpinhotel/src/gs/dolp/common/jqgrid/domain/StandardJqgridResData.java
+++ 01Project/hotel/02DevelopmentArea/04Code/dolpinhotel/src/gs/dolp/common/jqgrid/domain/StandardJqgridResData.java
@@ -117,6 +117,23 @@ public class StandardJqgridResData implements ResponseData {
 		}
 	}
 
+	/**
+	 * 获取指定字段的值的数组
+	 * @param columnIndex
+	 * @return
+	 */
+	public String[] getArrValueOfTheColumn(int columnIndex) {
+		if (rows == null || rows.size() == 0) {
+			return null;
+		}
+		int length = rows.size();
+		String[] arrValue = new String[length];
+		for (int i = 0; i < length; i++) {
+			arrValue[i] = rows.get(i).getCell().get(columnIndex);
+		}
+		return arrValue;
+	}
+
 	public String toString() {
 		return Json.toJson(this);
 	}
