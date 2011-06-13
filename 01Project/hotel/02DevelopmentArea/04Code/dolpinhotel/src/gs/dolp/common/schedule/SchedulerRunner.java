7006380a2c0b5ec8476a1af7ecf59cf73628443f
conanca <imconan@gmail.com>
2011-06-13 18:28:49 +0800

    修改触发器为每小时一次

--- 01Project/hotel/02DevelopmentArea/04Code/dolpinhotel/src/gs/dolp/common/schedule/SchedulerRunner.java
+++ 01Project/hotel/02DevelopmentArea/04Code/dolpinhotel/src/gs/dolp/common/schedule/SchedulerRunner.java
@@ -1,11 +1,9 @@
 package gs.dolp.common.schedule;
 
-import static org.quartz.DateBuilder.futureDate;
-import static org.quartz.JobBuilder.newJob;
-
 import java.util.Date;
 
-import org.quartz.DateBuilder.IntervalUnit;
+import org.quartz.DateBuilder;
+import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerFactory;
@@ -28,17 +26,17 @@ public class SchedulerRunner {
 		log.info("------- scheduler初始化完成 ------------------");
 
 		// 设置Job：CountOnlineUserJob
-		JobDetail job = newJob(CountOnlineUserJob.class).withIdentity("job1", "group1").build();
+		JobDetail job = JobBuilder.newJob(CountOnlineUserJob.class).withIdentity("job1", "group1").build();
 		// 设置开始时间为：5秒后
-		Date startTime = futureDate(5, IntervalUnit.SECOND);
+		Date startTime = DateBuilder.evenMinuteDate(new Date());
 		// 设置触发器：每30秒运行一次，无限运行
 		SimpleTrigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger_1", "group1").startAt(startTime)
-				.withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(30).repeatForever()).build();
+				.withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInHours(1).repeatForever()).build();
 		// job和trigger不存在于Scheduler，才加入调度
 		if (!sched.checkExists(job.getKey()) && !sched.checkExists(trigger.getKey())) {
 			sched.scheduleJob(job, trigger);
 		}
-		log.info(job.getKey() + "会重复: " + trigger.getRepeatCount() + " 次, 每 " + trigger.getRepeatInterval() / 1000
+		log.info(job.getKey() + " 将会重复: " + trigger.getRepeatCount() + " 次, 每 " + trigger.getRepeatInterval() / 1000
 				+ " 秒重复一次");
 
 		log.info("------- 正在启动 Scheduler -------------------");
