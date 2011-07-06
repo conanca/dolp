707956c5d021efb0eb6c99e04c5226fb1db5cc83
conanca <imconan@gmail.com>
2011-07-06 16:26:33 +0800

    开启权限验证功能

--- 01Project/dolp/02DevelopmentArea/04Code/dolp/src/gs/dolp/MainModule.java
+++ 01Project/dolp/02DevelopmentArea/04Code/dolp/src/gs/dolp/MainModule.java
@@ -2,6 +2,7 @@ package gs.dolp;
 
 import gs.dolp.common.view.DolpJsonViewMaker;
 import gs.dolp.system.filter.CheckLogon;
+import gs.dolp.system.filter.CheckPrivilege;
 
 import org.nutz.mvc.annotation.By;
 import org.nutz.mvc.annotation.Encoding;
@@ -20,9 +21,7 @@ import org.nutz.mvc.ioc.provider.JsonIocProvider;
 @SetupBy(MvcSetup.class)
 @Ok("json")
 @Fail("dolpjson")
-@Filters({ @By(type = CheckLogon.class)
-//, @By(type = CheckPrivilege.class) 
-})
+@Filters({ @By(type = CheckLogon.class), @By(type = CheckPrivilege.class) })
 @Views(DolpJsonViewMaker.class)
 public class MainModule {
 
