09d0c328054b0e2cf165f0ff8efc1b526df8e863
conanca <imconan@gmail.com>
2011-11-19 16:14:48 +0800

    修改该过滤器的注释及变量名

--- 01Project/dolp/02DevelopmentArea/04Code/dolp/src/com/dolplay/dolpbase/system/filter/CheckAttachmentOperation.java
+++ 01Project/dolp/02DevelopmentArea/04Code/dolp/src/com/dolplay/dolpbase/system/filter/CheckAttachmentOperation.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.nutz.dao.Dao;
 import org.nutz.dao.Sqls;
 import org.nutz.dao.sql.Sql;
 import org.nutz.dao.sql.SqlCallback;
@@ -19,14 +18,17 @@ import com.dolplay.dolpbase.common.domain.ExceptionAjaxResData;
 import com.dolplay.dolpbase.common.util.DaoProvider;
 import com.dolplay.dolpbase.system.domain.User;
 
+/**
+ * 对消息附件文件的操作的权限过滤器
+ * @author Administrator
+ */
 public class CheckAttachmentOperation implements ActionFilter {
 
 	@Override
 	public View match(ActionContext actionContext) {
+		// 获取当前用户所有的未删的所发消息或所收消息的附件文件的池Id，若该附件文件不在其中，则提示用户无权限
 		Object obj = actionContext.getRequest().getSession().getAttribute("logonUser");
 		Long userId = ((User) obj).getId();
-		Dao dao = DaoProvider.getDao();
-		// 获取当前用户所有的未删的所发消息或所收消息的附件文件的池Id
 		Sql sql = Sqls
 				.create("SELECT IDINPOOL FROM SYSTEM_POOLFILE WHERE ID IN(SELECT POOLFILEID FROM SYSTEM_MESSAGE_POOLFILE WHERE MESSAGEID IN (SELECT ID FROM SYSTEM_MESSAGE WHERE SENDERUSERID = $userId OR (ID IN (SELECT MESSAGEID FROM SYSTEM_MESSAGE_RECEIVERUSER WHERE USERID = $userId ) AND (STATE = 1 OR STATE = 2))))");
 		sql.vars().set("userId", userId);
@@ -39,11 +41,11 @@ public class CheckAttachmentOperation implements ActionFilter {
 				return ids;
 			}
 		});
-		dao.execute(sql);
+		DaoProvider.getDao().execute(sql);
 		@SuppressWarnings("unchecked")
 		List<Long> ids = (List<Long>) sql.getResult();
-		Long fileId = Long.parseLong(actionContext.getRequest().getParameter("id"));
-		if (!ids.contains(fileId)) {
+		Long fileIdInPool = Long.parseLong(actionContext.getRequest().getParameter("id"));
+		if (!ids.contains(fileIdInPool)) {
 			UTF8JsonView jsonView = new UTF8JsonView(null);
 			ExceptionAjaxResData excpAjaxResData = new ExceptionAjaxResData();
 			excpAjaxResData.setSystemMessage(null, null, "用户无权操作该文件!");
