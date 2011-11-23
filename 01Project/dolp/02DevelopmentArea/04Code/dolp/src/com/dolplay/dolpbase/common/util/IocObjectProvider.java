bafb8a7fd2b9509d7656def882551bfa02b21c86
conanca <imconan@gmail.com>
2011-11-23 18:26:54 +0800

    增强IocObjectProvider类的功能，更易于扩展

--- 01Project/dolp/02DevelopmentArea/04Code/dolp/src/com/dolplay/dolpbase/common/util/IocObjectProvider.java
+++ 01Project/dolp/02DevelopmentArea/04Code/dolp/src/com/dolplay/dolpbase/common/util/IocObjectProvider.java
@@ -1,7 +1,11 @@
 package com.dolplay.dolpbase.common.util;
 
+import javax.servlet.ServletContext;
+
 import org.nutz.filepool.FilePool;
 import org.nutz.ioc.Ioc;
+import org.nutz.mvc.ActionContext;
+import org.nutz.mvc.Mvcs;
 
 import com.dolplay.dolpbase.common.domain.DolpProperties;
 
@@ -11,7 +15,10 @@ public class IocObjectProvider {
 
 	private static FilePool attachmentPool;
 
+	private static Ioc ioc;
+
 	public static void init(Ioc ioc) {
+		IocObjectProvider.ioc = ioc;
 		prop = ioc.get(DolpProperties.class, "prop");
 		attachmentPool = ioc.get(FilePool.class, "attachmentPool");
 	}
@@ -22,5 +29,21 @@ public class IocObjectProvider {
 
 	public static FilePool getAttachmentPool() {
 		return attachmentPool;
+	}
+
+	public static <T> T getIocObject(ActionContext actionContext, Class<T> type, String name) {
+		return Mvcs.getIoc(actionContext.getServletContext()).get(type, name);
+	}
+
+	public static <T> T getIocObject(ServletContext context, Class<T> type, String name) {
+		return Mvcs.getIoc(context).get(type, name);
+	}
+
+	public static <T> T getIocObject(Ioc ioc, Class<T> type, String name) {
+		return ioc.get(type, name);
+	}
+
+	public static <T> T getIocObject(Class<T> type, String name) {
+		return ioc.get(type, name);
 	}
 }
