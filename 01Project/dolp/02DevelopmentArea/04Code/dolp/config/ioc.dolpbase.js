b6588f5fdd1558d16b6f9cfcafbc8fb8646348e5
conanca <imconan@gmail.com>
2011-12-14 17:34:10 +0800

    修改系统参数

--- 01Project/dolp/02DevelopmentArea/04Code/dolp/config/ioc.dolpbase.js
+++ 01Project/dolp/02DevelopmentArea/04Code/dolp/config/ioc.dolpbase.js
@@ -8,8 +8,8 @@ var ioc = {
 				"TestProp":"123",
 				"SYSTEM_SYSTEMNAME":"Dolp基盘",
 				"SYSTEM_ENVIRONMENT":"dev",
-				// 启动时初始化Dolp数据表
-				"SYSTEM_INITDOLPTABLES_ONSTART":false,
+				// 启动时初始化Dolp数据表(系统初次部署启动时可设置为true,第二次启动前应设为false)
+				"SYSTEM_INITDOLPTABLES_ONSTART":true,
 				// 启动Dolp调度任务
 				"SYSTEM_DOLPSCHEDULER_RUN":false,
 				// 是否检查入口方法存在于权限表中
