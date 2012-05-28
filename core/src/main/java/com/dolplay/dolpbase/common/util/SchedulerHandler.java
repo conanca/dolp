6d68fea36ac8ae7a68b59b86c558cad616740161
conanca <imconan@gmail.com>
2012-05-28 10:48:43 +0800

    SchedulerHandler 中捕获异常并处理(打印错误日志，返回空值)

--- core/src/main/java/com/dolplay/dolpbase/common/util/SchedulerHandler.java
+++ core/src/main/java/com/dolplay/dolpbase/common/util/SchedulerHandler.java
@@ -10,60 +10,94 @@ import org.quartz.SchedulerFactory;
 import org.quartz.Trigger;
 import org.quartz.TriggerKey;
 import org.quartz.impl.StdSchedulerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SchedulerHandler {
+	private static Logger logger = LoggerFactory.getLogger(SchedulerHandler.class);
 
-	public static Scheduler getScheduler() throws SchedulerException {
+	public static Scheduler getScheduler() {
 		SchedulerFactory sf = new StdSchedulerFactory();
-		Scheduler sched = sf.getScheduler();
+		Scheduler sched = null;
+		try {
+			sched = sf.getScheduler();
+		} catch (SchedulerException e) {
+			logger.error("调度任务异常", e);
+		}
 		return sched;
 	}
 
-	public static Date add(JobDetail job, Trigger trigger) throws SchedulerException {
+	public static Date add(JobDetail job, Trigger trigger) {
 		Date nextFireTime = trigger.getStartTime();
 
-		Scheduler sched = getScheduler();
-		if (!sched.checkExists(job.getKey()) && !sched.checkExists(trigger.getKey())) {
-			sched.scheduleJob(job, trigger);
-		} else {
-			nextFireTime = sched.getTrigger(trigger.getKey()).getNextFireTime();
+		Scheduler sched;
+		try {
+			sched = getScheduler();
+			if (!sched.checkExists(job.getKey()) && !sched.checkExists(trigger.getKey())) {
+				sched.scheduleJob(job, trigger);
+			} else {
+				nextFireTime = sched.getTrigger(trigger.getKey()).getNextFireTime();
+			}
+		} catch (SchedulerException e) {
+			logger.error("调度任务异常", e);
+			return null;
 		}
 
 		return nextFireTime;
 	}
 
-	public static void delete(JobDetail job) throws SchedulerException {
+	public static void delete(JobDetail job) {
 		Scheduler sched = getScheduler();
-		if (!sched.checkExists(job.getKey())) {
-			sched.deleteJob(job.getKey());
+		try {
+			if (!sched.checkExists(job.getKey())) {
+				sched.deleteJob(job.getKey());
+			}
+		} catch (SchedulerException e) {
+			logger.error("调度任务异常", e);
 		}
 	}
 
-	public static void pauseTrigger(TriggerKey key) throws SchedulerException {
+	public static void pauseTrigger(TriggerKey key) {
 		Scheduler sched = getScheduler();
-		if (!sched.checkExists(key)) {
-			sched.pauseTrigger(key);
+		try {
+			if (!sched.checkExists(key)) {
+				sched.pauseTrigger(key);
+			}
+		} catch (SchedulerException e) {
+			logger.error("调度任务异常", e);
 		}
 	}
 
-	public static void resumeTrigger(TriggerKey key) throws SchedulerException {
+	public static void resumeTrigger(TriggerKey key) {
 		Scheduler sched = getScheduler();
-		if (!sched.checkExists(key)) {
-			sched.resumeTrigger(key);
+		try {
+			if (!sched.checkExists(key)) {
+				sched.resumeTrigger(key);
+			}
+		} catch (SchedulerException e) {
+			logger.error("调度任务异常", e);
 		}
 	}
 
-	public static void pauseJob(JobKey key) throws SchedulerException {
+	public static void pauseJob(JobKey key) {
 		Scheduler sched = getScheduler();
-		if (!sched.checkExists(key)) {
-			sched.pauseJob(key);
+		try {
+			if (!sched.checkExists(key)) {
+				sched.pauseJob(key);
+			}
+		} catch (SchedulerException e) {
+			logger.error("调度任务异常", e);
 		}
 	}
 
-	public static void resumeJob(JobKey key) throws SchedulerException {
+	public static void resumeJob(JobKey key) {
 		Scheduler sched = getScheduler();
-		if (!sched.checkExists(key)) {
-			sched.resumeJob(key);
+		try {
+			if (!sched.checkExists(key)) {
+				sched.resumeJob(key);
+			}
+		} catch (SchedulerException e) {
+			logger.error("调度任务异常", e);
 		}
 	}
 }
