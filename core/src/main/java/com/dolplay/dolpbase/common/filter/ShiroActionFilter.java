f358ce77aae6302b5c6cc3c9e1116127ff6fcff0
conanca <imconan@gmail.com>
2012-12-14 15:49:39 +0800

    shirofilter增加日志

--- core/src/main/java/com/dolplay/dolpbase/common/filter/ShiroActionFilter.java
+++ core/src/main/java/com/dolplay/dolpbase/common/filter/ShiroActionFilter.java
@@ -2,14 +2,18 @@ package com.dolplay.dolpbase.common.filter;
 
 import java.lang.reflect.Method;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.aop.MethodInvocation;
 import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.subject.Subject;
 import org.nutz.lang.Lang;
 import org.nutz.mvc.ActionContext;
 import org.nutz.mvc.ActionFilter;
 import org.nutz.mvc.View;
 import org.nutz.mvc.view.UTF8JsonView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.dolplay.dolpbase.common.domain.AjaxResData;
 
@@ -19,6 +23,7 @@ import com.dolplay.dolpbase.common.domain.AjaxResData;
  *
  */
 public class ShiroActionFilter implements ActionFilter {
+	private static Logger logger = LoggerFactory.getLogger(ShiroActionFilter.class);
 
 	public View match(final ActionContext actionContext) {
 		try {
@@ -43,7 +48,14 @@ public class ShiroActionFilter implements ActionFilter {
 		} catch (AuthorizationException e) {
 			UTF8JsonView jsonView = new UTF8JsonView(null);
 			String permission = actionContext.getMethod().getAnnotation(RequiresPermissions.class).value()[0];
+			logger.warn("权限不足", e);
+			Subject s = SecurityUtils.getSubject();
 			jsonView.setData(AjaxResData.getInstanceErrorNotice("当前用户无该权限" + permission));
+			return jsonView;
+		} catch (Exception e) {
+			UTF8JsonView jsonView = new UTF8JsonView(null);
+			logger.warn("鉴权异常", e);
+			jsonView.setData(AjaxResData.getInstanceErrorNotice("鉴权异常"));
 			return jsonView;
 		}
 		return null;
